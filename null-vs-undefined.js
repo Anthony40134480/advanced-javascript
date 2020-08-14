51c6bff0bb2ce9dd1048ef39e536d12c37a86969
Anthony40134480 <anthonyjack421@gmail.com>
2020-08-14 18:37:32 +0300

    added undefined method

---
+++ null-vs-undefined.js
@@ -0,0 +1,18 @@
+// let pakhi;
+// console.log(pakhi);
+
+//if i dont return it will give me undefined
+function add(num1, num2){
+    return num1 + num2;
+}
+const result = add(5, 10);
+console.log(result);
+
+// if i dont have this property but i call this property
+const student = {name:'jack', id:5, class:12,}
+const {section} = student;
+console.log(section);
+
+// if i set value undefined
+const name = undefined;
+console.log(name);
